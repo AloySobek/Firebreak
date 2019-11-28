1eda8282482cbdb2510f23a267fd7f7906163c1c
AloySobek <super.rustamm@gmail.com>
2019-11-28 20:14:21 +0300

    Official documenation from Khronos.org has been added to learn myself nothing else

--- source/vkVulkanMethods.cpp
+++ source/vkVulkanMethods.cpp
@@ -6,11 +6,85 @@
 /*   By: Rustam <super.rustamm@gmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/14 13:55:17 by vrichese          #+#    #+#             */
-/*   Updated: 2019/11/26 20:49:26 by Rustam           ###   ########.fr       */
+/*   Updated: 2019/11/27 14:08:56 by Rustam           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vkClassContainer.hpp"
+
+namespace InstanceLayersExtensions
+{
+	std::vector<const char *> ppDesiredLayers =
+	{
+		//"VK_LAYER_LUNARG_api_dump",
+		//"VK_LAYER_LUNARG_core_validation",
+		//"VK_LAYER_KHRONOS_validation",
+		//"VK_LAYER_LUNARG_object_tracker",
+		//"VK_LAYER_LUNARG_standard_validation",
+		//"VK_LAYER_LUNARG_parameter_validation",
+		//"VK_LAYER_GOOGLE_threading",
+		//"VK_LAYER_GOOGLE_unique_objects"
+	};
+	std::vector<const char *> ppDesiredExtensions =
+	{
+		"VK_KHR_surface",
+		"VK_EXT_debug_utils",
+		"VK_MVK_macos_surface",
+		"VK_EXT_swapchain_colorspace",
+		"VK_KHR_get_surface_capabilities2",
+		"VK_KHR_get_physical_device_properties2",
+		"VK_KHR_device_group_creation",
+		"VK_KHR_device_group_creation",
+		"VK_KHR_get_physical_device_properties2",
+		"VK_KHR_get_surface_capabilities2",
+		"VK_KHR_surface",
+		"VK_EXT_debug_report",
+		"VK_EXT_debug_utils",
+		"VK_EXT_swapchain_colorspace",
+		"VK_MVK_macos_surface",
+		"VK_KHR_16bit_storage",
+		"VK_KHR_8bit_storage",
+		"VK_KHR_bind_memory2",
+		"VK_KHR_dedicated_allocation",
+		"VK_KHR_descriptor_update_template",
+		"VK_KHR_device_group",
+		"VK_KHR_get_memory_requirements2",
+		"VK_KHR_image_format_list",
+		"VK_KHR_maintenance1",
+		"VK_KHR_maintenance2",
+		"VK_KHR_maintenance3",
+		"VK_KHR_push_descriptor",
+		"VK_KHR_relaxed_block_layout",
+		"VK_KHR_sampler_mirror_clamp_to_edge",
+		"VK_KHR_shader_draw_parameters",
+		"VK_KHR_shader_float16_int8",
+		"VK_KHR_storage_buffer_storage_class",
+		"VK_KHR_swapchain",
+		"VK_KHR_swapchain_mutable_format",
+		"VK_KHR_uniform_buffer_standard_layout",
+		"VK_KHR_variable_pointers",
+		"VK_EXT_debug_marker",
+		"VK_EXT_host_query_reset",
+		"VK_EXT_scalar_block_layout",
+		"VK_EXT_shader_viewport_index_layer",
+		"VK_EXT_vertex_attribute_divisor",
+		"VK_EXTX_portability_subset",
+		"VK_AMD_gpu_shader_half_float",
+		"VK_AMD_negative_viewport_height",
+		"VK_INTEL_shader_integer_functions2",
+		"VK_NV_glsl_shader",
+	};
+};
+
+namespace PhysicalDeviceLayersExtensions
+{
+	std::vector<const char *> ppDesiredLayers =
+	{
+	};
+	std::vector<const char *> ppDesiredExtensions =
+	{
+	};
+}
 
 void Vulkan::run(GLFWwindow *pWindow)
 {
@@ -81,30 +155,13 @@ void Vulkan::vkSetupInstanceLayers()
 
 bool Vulkan::vkIsLayerSuitable(VkLayerProperties sLayer)
 {
-	char choose;
-
-	if (ALL_LAYER_OFF)
-		return (false);
-	else if (ONE_LAYER_MODE)
-	{
-		if (!strcmp(sLayer.layerName, "VK_LAYER_KHRONOS_validation"))
+	for (int i = 0; i < InstanceLayersExtensions::ppDesiredLayers.size(); ++i)
+		if (!strcmp(InstanceLayersExtensions::ppDesiredLayers[i], sLayer.layerName))
 			return (true);
-		else
-			return (false);
-	}
-	else if (USER_CHOOSE)
-	{
-		std::cout << "Next Layer suggest to apply to your project: " << sLayer.layerName << std::endl;
-		std::cout << "Do you wish to apply it? (Y / N)\n";
-		for (choose = '!'; choose != 'Y' and choose != 'N' and choose != 'y' and choose != 'n'; std::cin >> choose)
-			;
-		if (choose == 'Y' || choose == 'y')
+	for (int i = 0; i < PhysicalDeviceLayersExtensions::ppDesiredLayers.size(); ++i)
+		if (!strcmp(PhysicalDeviceLayersExtensions::ppDesiredLayers[i], sLayer.layerName))
 			return (true);
-		else
-			return (false);
-	}
-	else
-		return (true);
+	return (false);
 }
 
 void Vulkan::vkSetupInstanceExtensions()
@@ -125,8 +182,12 @@ void Vulkan::vkSetupInstanceExtensions()
 
 bool Vulkan::vkIsExtensionSuitable(VkExtensionProperties sExtension)
 {
-	if (strcmp(sExtension.extensionName, "VK_AMD_negative_viewport_height"))
-		return (true);
+	for (int i = 0; i < InstanceLayersExtensions::ppDesiredExtensions.size(); ++i)
+		if (!strcmp(InstanceLayersExtensions::ppDesiredExtensions[i], sExtension.extensionName))
+			return (true);
+	for (int i = 0; i < PhysicalDeviceLayersExtensions::ppDesiredExtensions.size(); ++i)
+		if (!strcmp(PhysicalDeviceLayersExtensions::ppDesiredExtensions[i], sExtension.extensionName))
+			return (true);
 	return (false);
 }
 
@@ -225,6 +286,7 @@ void Vulkan::vkSetupPhysicalDeviceExtensions()
 		{
 			++selectedPhysicalDeviceExtensionsCount;
 			ppAvailablePhysicalDeviceExtensionsNames[j++] = pAvailablePhysicalDeviceExtensions[i].extensionName;
+			std::cout << pAvailablePhysicalDeviceExtensions[i].extensionName << "\n";
 		}
 	}
 }
@@ -880,5 +942,5 @@ void Vulkan::vkDrawFrame()
 
 void	Vulkan::fbCleanUp()
 {
-	vkDestroyInstance(instanceProperties.instanceObject, nullptr);
+	;//vkDestroyInstance(instanceProperties.instanceObject, nullptr);
 }
